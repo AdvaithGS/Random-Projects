f9473638ed85ce7396b1c74367a5ab6896ddcf40
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-02 20:50:05 +0530

    changed password to have clipboard copy feature

--- password.py
+++ password.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import random
+import pyperclip
 password = ''
 while len(password) != 24:
     password += random.choice("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
 print(password)
-=======
-import random
-password = ''
-while len(password) != 24:
-    password += random.choice("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
-print(password)
->>>>>>> 4e1f9a33535211cf55ff7ba54045b635bc25a0ab
+pyperclip.copy(password)
+print('Copied to clipboard')
