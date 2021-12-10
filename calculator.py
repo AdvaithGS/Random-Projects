854b0bf5cdd9e17620c2975a9044c7d890d442b1
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-10 16:47:38 +0530

    added inverse functions

--- calculator.py
+++ calculator.py
@@ -1,58 +1,22 @@
 from math import *
 from time import sleep
 while True:
-    choice = input('''1 for addition,\n2 for subtraction,\n3 for multiplication,\n4 for division,\n5 for exponentiation,\n6 for trigonometry,\n0 to exit: \n--> ''')
-    if choice == 1:
-        while True:     
-            choice = input('''Float: 1,\nInteger: 2,\nBack: 0\n--> ''')
-            if choice == '1':
-                print(float(input('First number: '))+float(input('Second number: ')))
-                break
-            elif choice == '2':
-                print(int(input('First number: '))+int(input('Second number: ')))
-                break
-            elif choice == '0':
-                break
-            sleep(1)
+    choice = input('''1 for addition,\n2 for subtraction,\n3 for multiplication,\n4 for division,\n5 for exponentiation,\n6 for trigonometry (regular),\n7 for trigonometry (inverse)\n0 to exit: \n--> ''')
+    if choice == '1':
+        print(float(input('First number: '))+float(input('Second number: ')))
+        sleep(2)
     elif choice == '2':
-        while True:
-            choice = input('''Float: 1,\nInteger: 2,\nBack: 0\n--> ''')
-            if choice == '1':
-                print(float(input('First number: '))-float(input('Second number: ')))
-                break
-            elif choice == '2':
-                print(int(input('First number: '))-int(input('Second number: '))) 
-                break
-            elif choice == '0':
-                break
-            sleep(1)
+        print(float(input('First number: ')) - float(input('Second number: ')))
+        sleep(2)
     elif choice == '3':
-        while True:
-            choice = input('''Float: 1,\nInteger: 2,\nBack: 0\n--> ''')
-            if choice == '1':
-                print(float(input('First number: '))*float(input('Second number: ')))
-                break
-            elif choice == '2':
-                print(int(input('First number: '))*int(input('Second number: '))) 
-                break
-            elif choice == '0':
-                break
-                sleep(1)
+        print(float(input('First number: '))*float(input('Second number: ')))
+        sleep(2)
     elif choice == '4':
-        while True:    
-            choice = input('''Float Divide: 1,\nInteger Divide: 2,\nBack: 0\n--> ''')
-            if choice == '1':
-                print(float(input('First number: '))/float(input('Second number: ')))
-                break
-            elif choice == '2':
-                print(int(input('First number: '))//int(input('Second number: '))) 
-                break
-            elif choice == '0':
-                break
-                sleep(1)
+        print(float(input('First number: '))/float(input('Second number: ')))
+        sleep(2)
     elif choice == '5':
-        print(float(input('First number: '))**float(input('Second number: '))) 
-        sleep(1)
+        print(float(input('First number: '))**float(input('Second number: ')))
+        sleep(2)
     elif choice == '6':
         while True:
             choice = input('''Sin: 1,\nCos: 2,\nTan: 3\nBack: 0\n--> ''')
@@ -109,9 +73,47 @@ while True:
                         break
             elif choice == '0':
                 break
-            sleep(1)
+            sleep(2)
+    elif choice == '7':
+        while True:
+                choice = input('''Sin (inverse): 1,\nCos (inverse): 2,\nTan (inverse): 3,\nExit: 0\n--> ''')
+                if choice == '1':
+                    while True:
+                        choice = choice = input('''Radians Gang: 1,\nDegrees Gang: 2,\nExit: 0\n--> ''')
+                        if choice == '1':
+                            print(asin(int(input('Sine (inverse): enter value: '))))
+                            break
+                        elif choice == '2':
+                            print(degrees(asin(int(input('Sine (inverse): enter value: ')))))
+                            break
+                        elif choice == '0':
+                            break
+                elif choice == '2':
+                    while True:
+                        choice = choice = input('''Radians Gang: 1,\nDegrees Gang: 2,\nExit: 0\n--> ''')
+                        if choice == '1':
+                            print(acos(int(input('Cosine (inverse): enter value: '))))
+                            break
+                        elif choice == '2':
+                            print(degrees(acos(int(input('Cosine (inverse): enter value: ')))))
+                            break
+                        elif choice == '0':
+                            break
+                elif choice == '3':
+                    while True:
+                        choice = choice = input('''Radians Gang: 1,\nDegrees Gang: 2,\nExit: 0\n--> ''')
+                        if choice == '1':
+                            print(atan(int(input('Tangent (inverse): enter value: '))))
+                            break
+                        elif choice == '2':
+                            print(degrees(atan(int(input('Tangent (inverse): enter value: ')))))
+                            break
+                        elif choice == '0':
+                            break
+                elif choice == '0':
+                    break
     elif choice == '0':
         break
     elif choice == '42':
         print('The number of the universe and everything.....I see youre a person of culture')
-        sleep(1)
+        sleep(2)
