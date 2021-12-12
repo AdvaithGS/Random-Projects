78c864c50cdcddad5e1de1e8b7bafd8f7134eee9
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-12 17:16:34 +0530

    ran it for 2 hours and nothing

--- random-youtube-video.py
+++ random-youtube-video.py
@@ -5,14 +5,18 @@ url = 'https://www.youtube.com/watch?v='
 
 for i in range(11):
     url += choice(alphabet)
-
+remove = []
 while True:
     url = 'https://www.youtube.com/watch?v='
     for i in range(11):
         url += choice(alphabet)
+    if url in remove:
+        continue
     text = get(url).text
     if not "Video unavailable" in text:
         print(url,"Video unavailable" in text)
         break
+    else:
+        remove.append(url) 
 
 print(url,"Video unavailable" in get(url).text)
