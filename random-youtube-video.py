777d431c2aa8b9366dd15af2c56f5d7d155a3220
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-12 14:27:59 +0530

    got an error in using chr due to [ ]

--- random-youtube-video.py
+++ random-youtube-video.py
@@ -1,10 +1,11 @@
-from random import randrange
+from random import choice
 from requests import get
-import requests
+alphabet = 'qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
 url = 'https://www.youtube.com/watch?v='
 for i in range(11):
-    url += str(chr(randrange(65,123)))
+    url += str(choice(alphabet))
 while get(url).text.find("This video isn't available anymore") != -1:
     url = 'https://www.youtube.com/watch?v='
     for i in range(11):
         url += str(chr(randrange(65,123)))
+print(url)
