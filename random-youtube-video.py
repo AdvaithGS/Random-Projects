7e8a45a37ad8d3f2ebc04e5497f76357b3c89f37
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-12 14:24:43 +0530

    fixing video not found

--- random-youtube-video.py
+++ random-youtube-video.py
@@ -4,8 +4,7 @@ import requests
 url = 'https://www.youtube.com/watch?v='
 for i in range(11):
     url += str(chr(randrange(65,123)))
-while get('https://www.youtube.com/watch?v=_NCdAHCuqpd').text.find("This video isn't available any more") != -1:
+while get(url).text.find("This video isn't available anymore") != -1:
     url = 'https://www.youtube.com/watch?v='
     for i in range(11):
         url += str(chr(randrange(65,123)))
-print(url)
