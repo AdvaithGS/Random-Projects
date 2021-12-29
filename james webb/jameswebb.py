c6fc9d37be76acf635414e3eb111096e0afd73bb
= <advaith.gs4@gmail.com>
2021-12-29 18:49:21 +0530

    completed james webb, moving to astrobot

--- james webb/jameswebb.py
+++ james webb/jameswebb.py
@@ -1,25 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 from json import loads
+from image import get_image
 from time import strftime,gmtime
-
-from requests.hooks import dispatch_hook
-d = int(strftime('%d',gmtime()))
-s = strftime(f'%Y/%m/{d-7}-%H:48:00',gmtime()) 
-req = requests.get('https://jwst.nasa.gov/content/webbLaunch/whereIsWebb.html?s=09').text
-soup = BeautifulSoup(req,'lxml')
-page = soup.find_all('script')[6].text
-a = page[page.find(s)-100 :page.find(s)+230].replace(s,s.replace(':','/'))
-found = loads(a[a.index(',{')+1: a.rindex(',{') ].replace(',',',"').replace(':','":').replace('{','{"'))
-daysPassed = int(found['elapsedDays'])
-hoursPassed = round((float(found['elapsedDays'])%1)*24)
-elapsedtime = f'{daysPassed} days and {hoursPassed} hours'
-fromEarth = float(found['distanceTravelledKm']) + float(found['velocityKmSec'])*(int(strftime('%M'))-48)*60
-tol2 = round(1446320 - fromEarth,3)
-completion = round(fromEarth/1446320*100,3)
-image = str(soup.find('img',attrs = {'alt':'Image of the most recently started Deployment State of Webb - described in following text'}))
-
-image_link =  'https://jwst.nasa.gov/'+ image[image.rfind('src=') + 5 : image.rfind('/')]
-print(image_link)
-#print(elapsedtime,fromEarth,tol2,completion,sep = '\n')
-#distance between Earth and l2 -1446320  
+def get_james_webb():
+    d = int(strftime('%d',gmtime()))
+    s = strftime(f'%Y/%m/{d-7}-%H:48:00',gmtime()) 
+    req = requests.get('https://jwst.nasa.gov/content/webbLaunch/whereIsWebb.html?s=09').text
+    soup = BeautifulSoup(req,'lxml')
+    page = soup.find_all('script')[6].text
+    a = page[page.find(s)-100 :page.find(s)+230].replace(s,s.replace(':','/'))
+    #print(a[a.index('{'): a.rindex('}') +1].replace(',',',"').replace(':','":').replace('{','{"').replace(':.',':0.'))
+    found = loads(a[a.index('{'): a.rindex('}') +1].replace(',',',"').replace(':','":').replace('{','{"').replace(':.',':0.'))
+    daysPassed = int(found['elapsedDays'])
+    hoursPassed = round((float(found['elapsedDays'])%1)*24)
+    elapsedtime = f'{daysPassed} days and {hoursPassed} hours'
+    fromEarth = float(found['distanceTravelledKm']) + float(found['velocityKmSec'])*(int(strftime('%M'))-48)*60
+    tol2 = round(1446320 - fromEarth,3)
+    completion = round(fromEarth/1446320*100,3)
+    return elapsedtime,fromEarth,tol2,completion,get_image()
+    #distance between Earth and l2 -1446320     
