d0afec97bb20add46df2ef16727dc49a1fefe266
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-06 17:17:42 +0530

    I think ive fixed it

--- pwnedpass.py
+++ pwnedpass.py
@@ -3,15 +3,16 @@
 import hashlib
 from requests import get
 s = input('Enter password: ')
-x = hashlib.sha1(s.encode()).hexdigest().capitalize()
-
+x = str(hashlib.sha1(s.encode()).hexdigest().upper())
+print(x)
 req = get(f'https://api.pwnedpasswords.com/range/{x[:5]}').text.replace('\r','').split('\n')
 for i in range(len(req)):
     req[i] = req[i].split(':')
 req = dict(req)
 for i in req:
-    if i == x:
+    if i == x[5:]:
         print(f'Password Found: {s}')
+        print(i)
         print(f'Hash: {x}  Times Found: {req[i]}')
         break
 else:
