c7856fca9305ef16fee15e49acf85a2f39ad56f6
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-06 17:18:50 +0530

    minor changes

--- pwnedpass.py
+++ pwnedpass.py
@@ -1,10 +1,9 @@
 #Inspired by the Computerphile video featuring the Have I Been Pwned API 
 #Finding out whether your password has been found out in any leak
-import hashlib
+from hashlib import sha1
 from requests import get
 s = input('Enter password: ')
-x = str(hashlib.sha1(s.encode()).hexdigest().upper())
-print(x)
+x = str(sha1(s.encode()).hexdigest().upper())
 req = get(f'https://api.pwnedpasswords.com/range/{x[:5]}').text.replace('\r','').split('\n')
 for i in range(len(req)):
     req[i] = req[i].split(':')
