666d1cb0288c2d490ba3309487d30e41da9aa8c1
AdvaithGS <=>
2023-10-12 12:02:53 +0530

    partially working

--- sudoku.cpp
+++ sudoku.cpp
@@ -9,7 +9,7 @@
 std::list<int, std::allocator<int>> get_pos(int* arr,int i,int j){
  std::list<int> l = {1,2,3,4,5,6,7,8,9}; 
  for(int x = 0;x < 9;x++){
-  l.remove_if([arr,i,x,j](int k){return (k == *(arr + 9*i + x) or k == *(arr + 9*j + x)); });
+  l.remove_if([arr,i,x,j](int k){return (k == *(arr + 9*i + x) or k == *(arr + 9*x + j)); });
  };
  return l;
 }
@@ -24,27 +24,32 @@ int add(int j){
 };
 
 bool sudoku(int* map, int i, int j){
+ std::cout << "_"; 
  if(i == 9){
   return 1;
  }
  if ((*(map + (9*i) + j)) != 0){
   // (j + 1)%9  i + ((j+1)%9 < (j))
+  std::cout << "asdas";
   return sudoku(map,i + add(j), (j+1)%9 );
  }else{
   std::list<int, std::allocator<int>> l = get_pos(map,i,j);
   
   if (l.empty()){
+   std::cout << "back";
    return 0;
   }
   
   for( int k : l){
    *(map + (9*i) + j) = k;
    if ((sudoku(map,i + add(j), (j+1)%9 )) == 1){
-    std::cout << i << j << k;
     return 1;
    }else{
     *(map + (9*i) + j) = 0;
     continue;
+   }
+   if(k == l.back()){
+    std::cout << "Crisis: " << i << j << ": " << k;
    }
   }
  return 1;
@@ -60,13 +65,11 @@ int main(){
  sudoku(*map,0,0);
  int* i = *map;
  for(int j = 0;j< 81;j++){
-  if(j%27 == 0){
-   std::cout << "\n----------------------\n"; 
+  if(j%9 == 0){
+    std::cout << '\n';
   };
   std::cout << *i << ' ';
   i ++;
-  if((j+1)%3 == 0){
-    std::cout << "| ";
-  };
  }
+  std::cout << '\n'; 
 }
