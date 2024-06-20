41391b308892eec2925d47f6ed7b1fc691192afd
= <=>
2024-06-21 03:20:31 +0530

    moved more

--- password.py
+++
@@ -1,9 +0,0 @@
-import random
-import pyperclip
-length = int(input('Max length?(24 is the usual): '))
-password = ''
-while len(password) != length:
-    password += random.choice("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
-print(password)
-pyperclip.copy(password)
-print('Copied to clipboard')
